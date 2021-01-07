9d73da82f726a0586efafce9af46a5fe8c704690
Marcus Weinberger <marcusjw.ftw@gmail.com>
2021-01-07 16:28:32 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# flask-replit-pwa
+
+A modified version of [flask-pwa](https://github.com/umluizlima/flask-pwa) to run on [replit](https://repl.it).
+
+---
+
+[![Run on Repl.it](https://repl.it/badge/github/AgeOfMarcus/flask-replit-pwa)](https://repl.it/github/AgeOfMarcus/flask-replit-pwa)
---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(
+    __name__,
+    instance_relative_config=True,
+    static_url_path=''
+)
+app.config.from_mapping(
+    SECRET_KEY=os.environ.get('FLASK_SECRET'),
+)
+CORS(app)
+
+from app.controller import (
+    main, pwa
+)
+app.register_blueprint(main.bp)
+app.register_blueprint(pwa.bp)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
